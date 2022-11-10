cdfadbfc2acff66fd3012082423a68122a7adb0d
Miyakawa-99 <haru19970409@gmail.com>
2022-11-11 01:34:26 +0900

    update

--- matching_v1.py
+++ matching_v1.py
@@ -9,13 +9,14 @@ import networkx as nx, matplotlib.pyplot as plt
 from ortoolpy import networkx_draw
 import csv
 
-tbn = pd.read_csv('data/input/survey.csv')
-tbe = pd.read_csv('data/input/node0.csv')
-tbn['capacity'] = 0
-g = graph_from_table(tbn, tbe)[0]
-g = nx.Graph()
+tbe = pd.read_csv('data/input/survey.csv')
+tbn = pd.read_csv('data/input/node0.csv')
+tbe.insert(2, "capacity", 1)
+ntbe = tbe.rename(columns={'respondent': 'node1', 'target': 'node2'})
+
+g = graph_from_table(tbn, ntbe)[0]
 d = nx.max_weight_matching(g)
 pos = networkx_draw(g)
 nx.draw_networkx_edges(g, pos, width=3, edgelist=[(i, j) for i, j in d])
-print(d)
+
 plt.show()
