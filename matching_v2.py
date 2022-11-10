57b51f2f93dd21e83a079dd65119e8da05208c2e
Miyakawa-99 <haru19970409@gmail.com>
2022-11-11 02:01:44 +0900

    delete comment

--- matching_v2.py
+++ matching_v2.py
@@ -6,15 +6,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # 2グループに分ける
-# 12人の受講者はどちらかのグループに所属(ちょうど半分でないと機能しない)
+# 12人の受講者はどちらかのグループに所属
 # TODO: UPDATE students
 students = [
-        {"id": 1, "group": "B"},
-        {"id": 2, "group": "A"},
+        {"id": 1, "group": "A"},
+        {"id": 2, "group": "B"},
         {"id": 3, "group": "B"},
         {"id": 4, "group": "A"},
         {"id": 5, "group": "A"},
-        {"id": 6, "group": "A"},
+        {"id": 6, "group": "B"},
         {"id": 7, "group": "B"},
         {"id": 8, "group": "A"},
         {"id": 9, "group": "B"},
@@ -38,15 +38,12 @@ df = pd.read_csv('data/input/survey.csv')
 for i in groupA_ids:
   for j in groupB_ids:
     weight = 0
-    print("respondent" + str(i))
-    print("target" + str(j))
     item = df[(df['respondent'] == i) & (df['target'] == j)]
     rev_item = df[(df['respondent'] == j) & (df['target'] == i)]
     if not item.empty:
         weight = weight + int(item.loc[:, "weight"])
     if not rev_item.empty:
         weight = weight + int(rev_item.loc[:, "weight"])
-    print(str(weight))
     g.add_edge(i, j, weight = weight)
 
 d = nx.max_weight_matching(g)
